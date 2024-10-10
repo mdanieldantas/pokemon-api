2ad7e3930299afe9f4db7a35a7f8e356d276d3f9
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-10-10 16:23:00 -0300

    Refactorizar estilos e remover código não utilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react"
 
+async function fetchPokemon() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
+  const data = await response.json()
+  return data.results
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
 
 
-  return (
-    <>
-      
-    </>
-  )
+  useEffect(() => {
+    fetchPokemon().then(result => {
+      console.log("Requisição realizada")
+      console.log(result)
+      setPokemon(result)
+    })
+  }, [])
+
+// ...
+
+return (
+  <div className="app">
+    <div>
+      <h2>Pokémon</h2>
+      <ul className="pokemon">
+        {pokemon.map(mon => (
+          <li key={mon.name}>
+            <span>{mon.name}</span>
+            <button>
+              Ver detalhes
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+)
 }
 
 export default App
